f53ea71cd47ed2328016d1fc55437ffced018b50
DicksonOndaraNyabochwa <yungondara@gmail.com>
2025-03-12 14:10:11 +0300

    complete scope lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'; // Global variable
+var bestCustomer; // Declare bestCustomer in global scope
+const leastFavoriteCustomer = 'someone'; // Declare leastFavoriteCustomer as a constant
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // This will cause an error si nce leastFavoriteCustomer is a const
+}
+
+
+
+
